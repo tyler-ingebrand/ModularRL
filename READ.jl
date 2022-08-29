3955144df3fcf1fc6a4dffa7bbbbd1b2e9527fce
undergrad <undergrad@asg-a32252.oden.utexas.edu>
2022-08-29 14:50:48 -0500

    init

---
+++ READ.jl
@@ -0,0 +1,58 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 820d54e9-ebf4-4d90-9b7f-e2065f8f728d
+md"""
+# Modular RL
+
+## Introduction
+
+This project seeks to solve Markov Decision Processes (MDP) in a modular way. To do so, this project will offer one framework to support classical RL, classical control, multi-agent RL, hierarchical RL, and compositional RL. Doing so allows the end user to 1) understand RL in a modular way and 2) solve complex MDPs that would normally be unsolvable with classical RL approaches.
+
+Please read the following descriptions of each type of RL and how it fits into the framework.
+
+"""
+
+# ╔═╡ ec42f40a-3013-4438-8790-d0e31094c298
+md"""
+## Classical RL
+
+"""
+
+# ╔═╡ 742af376-5985-4dc4-a558-bb187b0e6e5a
+md"""
+# Explainable AI
+"""
+
+# ╔═╡ 94841fd3-c37a-4f85-90f8-aa44ab371bc8
+md"""
+
+![Internet required to see images](https://github.com/tyler-ingebrand/ModularRL/blob/master/docs/images/Cat.jpeg?raw=true")
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─820d54e9-ebf4-4d90-9b7f-e2065f8f728d
+# ╠═ec42f40a-3013-4438-8790-d0e31094c298
+# ╠═742af376-5985-4dc4-a558-bb187b0e6e5a
+# ╠═94841fd3-c37a-4f85-90f8-aa44ab371bc8
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
