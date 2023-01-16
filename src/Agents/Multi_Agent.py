1b0659d53c6d3bdc3e0d0ea2cb299487f4093f22
Sophia Smith <smithsophia1688#gmail.com>
2023-01-15 20:32:13 -0600

    added hook for tracking successful agents

--- src/Agents/Multi_Agent.py
+++ src/Agents/Multi_Agent.py
@@ -28,8 +28,10 @@ class Multi_Agent(Abstract_Agent):
 
     # The main function to learn from data. At a high level, takes in a transition (SARS) and should update the function
     # ocassionally updates the policy, but always stores transition
+
     def learn(self, state, action, reward, next_state, done, truncated, info, extras, tag = "1"):
         # update hook
+        # print("reward is", reward)
         self.hook.observe(self, state, action, reward, done,truncated, info, tag)
 
         # Update all agents
